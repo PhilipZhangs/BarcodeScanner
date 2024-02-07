9fdd02370352ffa01eaac10b38e8ee1614273298
PhilipZhangs <zhangsphilip@gmail.com>
2024-02-06 21:35:13 -0800

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by 张胖子 on 2024-02-06.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
